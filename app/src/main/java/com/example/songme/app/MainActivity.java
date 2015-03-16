13a277b014fe17c642a02fa70e94bddf013db66f
SongMiLee <s91010@naver.com>
2015-03-16 15:36:49 +0900

    my commit

---
+++ app/src/main/java/com/example/songme/app/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.songme.app;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+        2.5초 뒤에 로그인 화면으로 가기 위한 코드
+        * */
+        Handler handler=new Handler();
+        Runnable myRunnable =new Runnable(){
+
+            @Override
+            public void run() {
+                Intent main=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(main);
+                finish();
+            }
+        };
+        handler.postDelayed(myRunnable,2500);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
