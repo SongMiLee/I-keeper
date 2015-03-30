80bb6b420bc6dedbdec547e445928ad87a87b972
SongMiLee <s91010@naver.com>
2015-03-30 17:24:59 +0900

    adjust the login

--- i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/MainActivity.java
+++ i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/MainActivity.java
@@ -3,7 +3,6 @@ package org.androidtown.i_keeper_test;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,14 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.nio.Buffer;
-
 
 public class MainActivity extends Activity {
     //네트워크 연결을 위한 변수들
@@ -27,15 +23,14 @@ public class MainActivity extends Activity {
     PrintWriter writer;
     BufferedReader reader;
 
-    //네트워크를 연결하기 위한 핸들러 Thread를 extends 한다.
+    //네트워크를 작업을하기 위한 핸들러 Thread를 extends 한다.
     SocketHandler sh;
-    ReaderHandler rh;
 
     //editText에서의 값을 가지기 위한 변수
     String text;
     EditText edttext;
 
-    String page="t";
+    String page="false";
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +38,10 @@ public class MainActivity extends Activity {
             setContentView(R.layout.activity_main);
             startActivity(new Intent(this, SplashActivity.class));
 
+
             //소켓 핸들러 쓰레드를 생성 후 실행
             sh=new SocketHandler();
             sh.start();
-
 
             //UI의 값을 얻어서 저장
             edttext=(EditText)findViewById(R.id.TextView02);
@@ -56,7 +51,6 @@ public class MainActivity extends Activity {
             login_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 text= edttext.getText().toString();
-                Log.i("Text=", text);
                 //id 부분이 null 인 경우
                 if(text.isEmpty() || text.length()==0){
                     Toast.makeText(getApplicationContext(),"ID를 입력하세요",Toast.LENGTH_SHORT).show();
@@ -64,23 +58,32 @@ public class MainActivity extends Activity {
                 //id 부분이 null이 아닌 경우
                 else
                 {
-                    //로그인 id 값을 서버에 보낸 후 버퍼를 비운다.
+                    //로그인 id 값을 버퍼에 바로 작성하도록 한다.
                     writer.println("_login=" + text);
                     writer.flush();
 
-                    rh=new ReaderHandler();
-                    rh.start();
-
-                    Log.i("page : ", page);
-
-                    if(page.equals("true"))
-                        startActivity(new Intent(MainActivity.this, MonitorActivity.class));
-
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try{
+                                while(true) {
+                                    String tmp;
+                                    tmp=reader.readLine();
+                                    if(!tmp.isEmpty())
+                                        page=tmp;
+                                }
+                            }catch(Exception e){}
+                        }
+                    }).start();
+                    Log.i("page : ",page);
+                    if(page.equals("true")){
+                        Toast.makeText(getApplicationContext(),"login",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this,MonitorActivity.class));
+                    }else
+                        Toast.makeText(getApplicationContext(),"no id",Toast.LENGTH_SHORT).show();
                 }
              }
         });
-
-
     }
 
     class SocketHandler extends Thread{
@@ -89,20 +92,8 @@ public class MainActivity extends Activity {
                 socket=new Socket("104.155.212.106",port);//서버의 9999 포트로 접속
                 writer=new PrintWriter(socket.getOutputStream());
                 reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
             } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    class ReaderHandler extends Thread{
-        public void run(){
-            try{
-                //page=reader.readLine();
-                while((page=reader.readLine())!=null)
-                    System.out.println(page);
-            }catch(Exception e){
                 e.printStackTrace();
             }
         }
