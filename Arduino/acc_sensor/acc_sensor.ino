d01676a6e1407621d25466b7e7838f7369946185
jeongdongwon <ch0703_kr@naver.com>
2015-04-02 22:54:25 +0900

    진동센서 초기작동소스 추가, 심박센서를 통한 심박박동구간 감지 추가

---
+++ Arduino/acc_sensor/acc_sensor.ino
@@ -0,0 +1,25 @@
+int s0 = A0;
+int s1 = A1;
+int s2 = A2;
+float x, y, z,r;
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+
+  x = analogRead(s0);
+  y = analogRead(s1);
+  z = analogRead(s2);
+  r=x*x+y*y+z*z;
+  Serial.println(r);
+ /* Serial.print(x);
+  Serial.print(", ");
+  Serial.print(y);
+  Serial.print(", ");
+  Serial.println(z);
+  */
+  delay(50);
+}
+
--- Arduino/pulse_sensor/pulse_sensor.ino
+++ Arduino/pulse_sensor/pulse_sensor.ino
@@ -1,16 +1,54 @@
 int Pulse_pin=A0; //심박센서를 A0(아날로그 포트번호)로 편하게 부르기 위해 변수값으로 설정.
-float pul_result; //측정값을 저장하는 변수
-float r;  //계산값을 저장하기 위한 변수
+int now_time;
+int prev_time;
+float pul_result = 0; //측정값을 저장하는 변수
+float thresh_hold = 0;
+float prev_value;
+float r = 0;  //계산값을 저장하기 위한 변수
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Waiting For Start.......");
+  prev_time = millis();  
+  
+  while(prev_time<10000){
+    pul_result=analogRead(Pulse_pin);  
+    if(thresh_hold < pul_result){
+      thresh_hold = pul_result;
+    }
+    prev_time = millis();
+    Serial.print("prev:");
+    Serial.print(prev_time);
+    Serial.print(",");
+    Serial.print("thresh:");
+    Serial.println(thresh_hold);
+  }  
+  thresh_hold = thresh_hold / 190;
+  thresh_hold = thresh_hold * thresh_hold * thresh_hold;
+  thresh_hold = thresh_hold * 0.77;
+  Serial.println(thresh_hold);
 }
 
 void loop() {
-  pul_result=analogRead(Pulse_pin)/250; //값의 미세한 차이를 증폭시키기 위한 계산들...
+  prev_value = r;
+  pul_result=analogRead(Pulse_pin)/190; //값의 미세한 차이를 증폭시키기 위한 계산들...
   r=pul_result*pul_result*pul_result; //
-
+  delay(10);
+  /*if(prev_value > r){
+    now_time = prev_time - millis();
+    Serial.println(now_time);
+    prev_time = millis();
+    delay(380);    
+  }*/
+  
+  if(r>thresh_hold){
+    now_time = prev_time - millis();
+    Serial.println(now_time);
+    prev_time = millis();
+    delay(380);    
+  }
+  /*Serial.print(prev_value);
+  Serial.print(" ");
   Serial.println(r);
-  //Serial.println(pul_result);
-  delay(20);
+  */
 }
