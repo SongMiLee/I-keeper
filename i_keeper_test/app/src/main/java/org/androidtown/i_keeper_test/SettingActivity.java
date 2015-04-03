33c060efe403a501a997ed764613b73a86ee8bfb
EunjiLim <cucumbers816@gmail.com>
2015-04-03 14:03:13 +0900

    add setting

--- i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/SettingActivity.java
+++ i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/SettingActivity.java
@@ -1,15 +1,19 @@
 package org.androidtown.i_keeper_test;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by 임은지 on 2015-04-02.
  */
-public class SettingActivity extends Activity {
+public class SettingActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Setting");
     }
 }
