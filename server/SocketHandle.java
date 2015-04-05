2485b7010ec1401e14a3fa6d411fbae03d34c362
SongMiLee <s91010@naver.com>
2015-04-05 04:24:07 +0000

    add the _Refresh

--- server/SocketHandle.java
+++ server/SocketHandle.java
@@ -29,14 +29,14 @@ public class SocketHandle extends Thread {
 
 		this.soc=s;
 		if(client_name==null){
-		client_name=new ArrayList<String>(); //
-		client_name.add(" ");
+			client_name=new ArrayList<String>(); //
+			client_name.add(" ");
 		}
 		System.out.println("list 초기화 완료.....");
 		try{
-		spw=new PrintWriter(soc.getOutputStream(),true);
-		sbr=new BufferedReader(new InputStreamReader(soc.getInputStream()));
-		System.out.println("spw,sbr 초기화 완료.....");
+			spw=new PrintWriter(soc.getOutputStream(),true);
+			sbr=new BufferedReader(new InputStreamReader(soc.getInputStream()));
+			System.out.println("spw,sbr 초기화 완료.....");
 		}catch(Exception e){}
 	}
 	
@@ -52,40 +52,66 @@ public class SocketHandle extends Thread {
 					String[] realinfo=information.split("=");
 					System.out.println("Login " + realinfo[1]);
 					
-			SQL = "select id from cloinfo where id='"+realinfo[1]+"';";			
-	                Class.forName(driverName); // 드라이버 로드
-	                Connection con  = DriverManager.getConnection(dbURL,"toor","toor"); // 연결
-	                System.out.println("Mysql DB Connection.");
+					SQL = "select id from cloinfo where id='"+realinfo[1]+"';";			
+	               			Class.forName(driverName); // 드라이버 로드
+			                Connection con  = DriverManager.getConnection(dbURL,"toor","toor"); // 연결
+	       			        System.out.println("Mysql DB Connection.");
 	               
-	                Statement stmt = con.createStatement();
-	                stmt.executeQuery(SQL);//execute the SQL Query
+	             			Statement stmt = con.createStatement();
+			                stmt.executeQuery(SQL);//execute the SQL Query
 	                
-	                ResultSet result = stmt.executeQuery(SQL);
-	                if(!result.next()){//If db didn't find the id
-	                	System.out.println("There is no id "+realinfo[1]);
-	                	spw.println("false");
-				spw.flush();
-	                }else{//db find the id
-	                	System.out.println("There is id "+realinfo[1]);
-	                	spw.println("true");
-				spw.flush();
+	               			ResultSet result = stmt.executeQuery(SQL);
+			                if(!result.next()){//If db didn't find the id
+	       			         	System.out.println("There is no id "+realinfo[1]);
+	                			spw.println("false");
+						spw.flush();
+	       			         }else{//db find the id
+	                			System.out.println("There is id "+realinfo[1]);
+			                	spw.println("true");
+						spw.flush();
 	                	
-	                }
-			con.close();
-			System.out.println("MySql DB disconnect");
+	               			 }
+					con.close();
 				}
-				else if(information.indexOf("_delete")!=-1)
+				else if(information.indexOf("_sensor")!=-1)
 				{
-					
+					System.out.println(information);
+					spw.println(information);
+					spw.flush();	
 				}
-				else if(information.indexOf("_refresh")!=-1)
+				else if(information.indexOf("_Refresh")!=-1)
 				{
+					String[] realinfo=information.split("=");
+					System.out.println("Refresh "+realinfo[1]);//clothes ID
+
+					SQL = "select id from pulse_sensor where id='"+realinfo[1]+"';";             
+ 
+                                        Class.forName(driverName); // 드라이버 로드
+                                        Connection con  = DriverManager.getConnection(dbURL,"toor","toor"); //DB Connection
+
+                                        System.out.println("Mysql DB Connection.");
+                                        Statement stmt = con.createStatement();
+                                        stmt.executeQuery(SQL);//execute the SQL Query
+                                        ResultSet result = stmt.executeQuery(SQL);
 					
 				}
 		
 			}catch(Exception e){e.printStackTrace();}
 		}
 	
+	}
+	void addList(String arg)
+	{
+		
+		for(int i=0; i<client_name.size(); i++)
+		{
+			//중복 검사
+			//System.out.println(arg);
+			if(client_name.get(i).matches(arg))
+				return ;
+		}
+		client_name.add(arg);
+		//client_name.add(arg);
 	}
 	
 }
