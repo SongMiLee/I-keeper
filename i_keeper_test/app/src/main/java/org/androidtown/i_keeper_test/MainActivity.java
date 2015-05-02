9620d7554925db4a4bb7a4287745e2d72e04a11f
SongMiLee <s91010@naver.com>
2015-05-02 14:39:09 +0900

    Http post

--- i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/MainActivity.java
+++ i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/MainActivity.java
@@ -13,32 +13,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
     //프로그래스 대화상자 객체
     ProgressDialog progressDialog;
-    //네트워크 연결을 위한 변수들
-    Socket socket;
-    int port =9999;
-    PrintWriter writer;
-    BufferedReader reader;
-
-    //네트워크를 작업을하기 위한 핸들러 Thread를 extends 한다.
-    SocketHandler sh;
-    Handler handler;
 
     //editText에서의 값을 가지기 위한 변수
-    String text;
+    String id;
     EditText edttext;
 
     String page="wait";
     //user의 id 정보를 저장하기 위한 클래스 변수
     UserInfo user;
+    Handler handler;
+    String line=null;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +45,6 @@ public class MainActivity extends Activity {
             setContentView(R.layout.activity_main);
             startActivity(new Intent(this, SplashActivity.class));
             user=new UserInfo();
-
-
-            //소켓 핸들러 쓰레드를 생성 후 실행
-            sh=new SocketHandler();
-            sh.start();
             handler=new Handler();
 
             //UI의 값을 얻어서 저장
@@ -60,53 +54,71 @@ public class MainActivity extends Activity {
              //버튼 클릭 이벤트
             login_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                text= edttext.getText().toString();
+                id= edttext.getText().toString();
                 //id 부분이 null 인 경우
-                if(text.isEmpty() || text.length()==0){
+                if(id.isEmpty() || id.length()==0){
                     Toast.makeText(getApplicationContext(),"ID를 입력하세요",Toast.LENGTH_SHORT).show();
                 }
                 //id 부분이 null이 아닌 경우
                 else
                 {
-             //       startActivity(new Intent(MainActivity.this,MonitorActivity.class));
-                 //로그인 id 값을 버퍼에 바로 작성하도록 한다.
-                    writer.println("_login=" + text);
-                    writer.flush();
+                    // startActivity(new Intent(MainActivity.this,MonitorActivity.class));
                     //프로그레스 다이얼로그를 띄운다.
                     showDialog(1001);
-                    //쓰레드에서 서버로 값이 오기를 기다린다.
-                    //서버에서 온 값이 wait이 아니라면 if 문을 실행 시켜 로그인 여부를 결정해 준다.
                     new Thread(new Runnable() {
                         @Override
                         public void run() {
+                            HttpClient client=new DefaultHttpClient();
+                            HttpPost getMethod=new HttpPost("http://192.168.25.6:8080/test/HelloWorld");
+
                             try{
-                                //서버로 부터 값이 넘어올 때 까지 대기.
-                                page=reader.readLine();
-                            }catch(Exception e){    e.printStackTrace(); }
+                                ArrayList<NameValuePair> NameValuePairs=new ArrayList<NameValuePair>(2);
+                                //보낼 값을 ArrayList에 키와 값으로 저장을 한다.
+                                NameValuePairs.add(new BasicNameValuePair("Mode","Login"));
+                                NameValuePairs.add(new BasicNameValuePair("id",id));
+
+                                getMethod.setEntity(new UrlEncodedFormEntity(NameValuePairs));
+                                //값을 보낸다.
+                                HttpResponse response = client.execute(getMethod);
+
+                                //값을 읽기 위한 cbr을 생성
+                                BufferedReader cbr=new BufferedReader(new InputStreamReader(response.getEntity().getContent(),"utf-8"));
+
+                                String res="";
+                                //null이 아닐때 값을 읽어서 res에 넣는다.
+                                while((line=cbr.readLine())!=null)
+                                    res+=line;
+                                System.out.println("result : "+res);
+
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
                             handler.post(new Runnable() {
                                 @Override
                                 public void run() {
-                                    if(!page.equals("wait"))//디폴트 값이 아니면 아래 2개의 if 문 중 하나를 실행
+                                    //id를 찾은 경우
+                                   if(page.equals("true"))
+                                   {
+                                        //프로그래스 창을 끈다.
+                                        if(progressDialog!=null)
+                                            progressDialog.dismiss();
+
+                                        //user 정보를 저장한 뒤 다음 액티비티로 넘어간다.
+                                        user.setUserID(id);
+                                        startActivity(new Intent(MainActivity.this,MonitorActivity.class));
+                                    }
+                                    //id를 못 찾은 경우
+                                    if(page.equals("false"))
                                     {
-                                        if(page.equals("true")){//id를 찾은 경우
-                                            //프로그래스 창을 끈다.
-                                            if(progressDialog!=null)
-                                                progressDialog.dismiss();
+                                        if(progressDialog!=null)
+                                            progressDialog.dismiss();
+                                        Toast.makeText(getApplicationContext(),"id가 없습니다.",Toast.LENGTH_SHORT).show();
 
-                                            //user 정보를 저장한 뒤 다음 액티비티로 넘어간다.
-                                            user.setUserID(text);
-                                            startActivity(new Intent(MainActivity.this,MonitorActivity.class));
-                                        }
-                                        if(page.equals("false")){//id를 못 찾은 경우
-                                            if(progressDialog!=null)
-                                                progressDialog.dismiss();
-                                            Toast.makeText(getApplicationContext(),"id가 없습니다.",Toast.LENGTH_SHORT).show();
-
-                                        }
                                     }
 
                                 }
                             });
+
                         }
                     }).start();
                 }
@@ -114,21 +126,7 @@ public class MainActivity extends Activity {
         });
     }
 
-    class SocketHandler extends Thread{
-        public void run(){
-            try {
-                socket=new Socket("104.155.212.106",port);//서버의 9999 포트로 접속
-                writer=new PrintWriter(socket.getOutputStream());
-                reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    public Dialog onCreateDialog(int id){
+   public Dialog onCreateDialog(int id){
         switch (id){
             case 1001 :
                 progressDialog=new ProgressDialog(this);
