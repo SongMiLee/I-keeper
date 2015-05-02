a5516c2249b1918812718a22c490163b1679d636
SongMiLee <s91010@naver.com>
2015-05-02 06:30:46 +0000

    server http

--- server/WEB-INF/classes/Server.java
+++ server/WEB-INF/classes/Server.java
@@ -1,58 +1,56 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 public class Server extends HttpServlet{
-	String test="Hello Server";
+	static String test="Hello Server";
 	String info;
 
 	//DB Access
 	String driverName="com.mysql.jdbc.Driver";
-   	String dbURL="jdbc:mysql://173.194.85.216:3306/ik";
+   	String dbURL="jdbc:mysql://173.194.85.216/ik";
 	String User="toor";
    	String Password="toor";
 	String SQL;
    	Connection conn;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		PrintWriter out=resp.getWriter();
+		PrintWriter out =resp.getWriter();
+
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<H1>"+test+"</H1>");
+		out.println("<H2>"+test+"</H2>");
 		out.println("</body>");
 		out.println("</html>");
+
 		out.close();
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 
-		ObjectInputStream oin = new ObjectInputStream(req.getInputStream());
-		ObjectOutputStream oos = new ObjectOutputStream(resp.getOutputStream());
-
+		PrintWriter spw=new PrintWriter(resp.getOutputStream());
 		try{
-
 		//read the value from stream
-		info=(String)oin.readObject();
-
+		info="test";
+		info=req.getParameter("Mode");
 		//init the db connection
                  Class.forName(driverName);
                  conn=DriverManager.getConnection(dbURL,User,Password);
 	         Statement stmt=conn.createStatement();
-		
-		//realinfo[0]=> command (if application, realinfo[1]=>id)
-		 String[] realinfo=info.split("=");
 	
 		//search the db	
-		 String result;
+		 String result="tmp";
 
-			if(realinfo[0].equals("_login")){
+			if(info.equals("Login")){
+			 String id=req.getParameter("id");
 
-			  SQL="select * from cloinfo where id="+realinfo[1];
+			  SQL="select * from cloinfo where id="+id;
 			  ResultSet rs=stmt.executeQuery(SQL);
 
 			  if(rs.next()!=false){
@@ -60,12 +58,14 @@ public class Server extends HttpServlet{
 			  }
 			  else
 				result="false";
+
+			  spw.write(result);
+			  spw.flush();
 			
-			  oos.writeObject(result); 
-			}
+			}/*
 			else if(realinfo[0].equals("_Refresh")){
 
-			  SQL="select * from pulse where id="+realinfo[1];
+			  SQL="select * from pulse where id="+];
 			  ResultSet rs=stmt.executeQuery(SQL);
 
 			  while(rs.next()){
@@ -74,7 +74,9 @@ public class Server extends HttpServlet{
 			     Timestamp time=rs.getTimestamp("time");
 
 			     result=time+"="+id+"="+data;
-			     oos.writeObject(result);
+			    
+			     spw.write(result);
+			     spw.flush();
 
 			  }
 			}
@@ -83,10 +85,9 @@ public class Server extends HttpServlet{
 			else if(realinfo[0].equals("_vibsensor")){
 			}
 			else if(realinfo[0].equals("_pulsesensor")){
-			}
+			}*/
 
 		}catch(Exception e) { e.printStackTrace(); }
-		finally{	oos.close(); }
-		oin.close();
+		finally{	 }
 	}
 }
