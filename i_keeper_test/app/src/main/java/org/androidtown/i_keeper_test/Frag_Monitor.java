0f37311bf1bd3f8584d84ed76d9fa706f250c749
JungEunHye <zxy816@naver.com>
2015-05-11 21:00:47 +0900

    frag_realtime revise

--- i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/Frag_Monitor.java
+++ i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/Frag_Monitor.java
@@ -3,6 +3,7 @@ package org.androidtown.i_keeper_test;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,7 +42,12 @@ public class Frag_Monitor extends Fragment {
     String str;
 
     //
+    //
+    private boolean running = false;
+    ProgressHandler handler;        //객체 생성
+    //
 
+    //
     public Frag_Monitor() {
         // Required empty public constructor
     }
@@ -69,43 +75,6 @@ public class Frag_Monitor extends Fragment {
         //
         output = (TextView) rootView.findViewById(R.id.output);
         btn_refresh = (Button) rootView.findViewById(R.id.btn_refresh);
-
-        btn_refresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //접속할 때 먼저 _Refresh를 보내 새로운 정보를 보내도록 한다.
-                socket_out.println("_Refresh="+user.returnUserID());
-                socket_out.flush();
-
-            }
-        });
-        Thread worker = new Thread() {
-            public void run() {
-                try {
-                    socket = new Socket("104.155.212.106", port);
-                    socket_out = new PrintWriter(socket.getOutputStream(), true);
-                    socket_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-
-
-                    while ((str=socket_in.readLine())!=null) {
-
-                        output.post(new Runnable() {
-                            public void run() {
-                                //"="로 값을 나눈다.
-                                String [] tmp=str.split("=");
-                                output.setText("VALUE  : "+tmp[0]+", "+tmp[1]+", "+tmp[2]);
-                            }
-                        });
-                    }
-                } catch (Exception e) {
-                }
-            }
-        };
-        worker.start();
 
 
 //
@@ -157,33 +126,12 @@ public class Frag_Monitor extends Fragment {
     private double getRandom() {
         return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
     }
-//
-  /*  class SocketHandle extends Thread{
-        public void run(){
-            try {
-                //소켓 관련 writer, reader를 초기화 시킨다.
-                socket=new Socket("104.155.212.106",port);//서버의 9999 포트로 접속
-                writer=new PrintWriter(socket.getOutputStream());
-                reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                //접속할 때 먼저 _Refresh를 보내 새로운 정보를 보내도록 한다.
-                writer.println("_Refresh="+user.returnUserID());
-                writer.flush();
 
-                //이 아래에 있는 소스의 위치는 바뀔 수 있다.
-                String str;
-                //서버에서 보낸 값을 읽는다.
-                while((str=reader.readLine())!=null){
-                    //"="로 값을 나눈다.
-                    String [] tmp=str.split("=");
-                    //디버그용으로 로그에 찍힘. ex) a, b, c
-                        System.out.println(tmp[0]+", "+tmp[1]+", "+tmp[2]);
-                }
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public class ProgressHandler extends Handler{ //Handler 클래스를 상속하여 새로운 핸들러 클래스를 정의
+        public void handleMessage(Message msg){
+            // 여기서 setTExt
 
         }
-    }*/
-
+    }
 }
--- i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/Frag_Realtime.java
+++ i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/Frag_Realtime.java
@@ -1,12 +1,10 @@
-package org.androidtown.i_keeper_test;
+﻿package org.androidtown.i_keeper_test;
 
 
 import android.os.Bundle;
-<<<<<<< HEAD
-=======
 import android.os.Message;
 import android.support.v4.app.Fragment;
->>>>>>> 3303cd871af0543efeadeab4e6ee8d507c5aabe0
+
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -94,26 +92,12 @@ public class Frag_Realtime extends Fragment {
         //  return inflater.inflate(R.layout.frag_realtime, container, false);
     }
 
-
-<<<<<<< HEAD
-        Thread worker = new Thread() {
-            public void run() {
-                // 메인 스레드가 처리해야 할 루틴
-
-                //HttpClient 인스턴스 생성
-                HttpClient client=new DefaultHttpClient();
-                // HTTP 메서드의 인스턴스 생성
-                // URL과 함께 HttpClient 컴포넌트에서 제공하는 HTTP 메서드의 클래스의 생성자를 사용하여
-                // HTTP 의 요청라인을 지원하는 HTTP 메시지의 인스턴스를 생성한다.
-                HttpPost getMethod=new HttpPost("http://alert-height-91305.appspot.com/hello");
+    public void onStart(){
+        super.onStart();
+        Thread thread1 = new Thread(new Runnable(){
+            public void run(){
 
                 try{
-                    // 서버에 시간데이터를 보내고 해당 시간에 대한 센서값들을 얻어 온다.
-                    String inTime = new SimpleDateFormat("HH:mm:ss").format(new java.util.Date());  // 현재 시간을 얻어옴(시,분,초)
-                    String []tmp = inTime.split(":");
-                    int time_hour = Integer.parseInt(tmp[0]);           // 현재 시간(시)
-                    int time_minute = Integer.parseInt(tmp[1]);     // 현재 시간(분)
-                    output.setText("현재시간  : "+time_hour+"시 "+time_minute+"분");
 
                     ArrayList<NameValuePair> NameValuePairs=new ArrayList<NameValuePair>(1);
                     //보낼 값을 ArrayList에 키와 값으로 저장을 한다.
@@ -125,75 +109,22 @@ public class Frag_Realtime extends Fragment {
                     //값을 읽기 위한 cbr을 생성
                     BufferedReader cbr=new BufferedReader(new InputStreamReader(response.getEntity().getContent(),"utf-8"));
                     while((readData=cbr.readLine())!=null){
-                                arr.add("value : "+readData);
+                        arr.add("value : "+readData);
                     }
-                    System.out.println("arr : "+ arr);
+                    System.out.println("arr : " + arr);
 
-                    t_sensor1.setText(arr.get(0));
-                    t_sensor2.setText(arr.get(1));
-                    t_sensor3.setText(arr.get(2));
-                    t_sensor4.setText(arr.get(3));
-                    t_sensor5.setText(arr.get(4));
-                    t_sensor6.setText(arr.get(5));
+                    //
 
+                    Message msg = handler.obtainMessage();
+                    handler.sendMessage(msg);
+
+                    //
 
                 } catch(Exception e){
                     e.printStackTrace();
                 }
 
-                mHandler.post(new Runnable(){
-                        @Override
-                        public void run(){
-                            t_sensor1.setText(arr.get(0));
-                            t_sensor2.setText(arr.get(1));
-                            t_sensor3.setText(arr.get(2));
-                            t_sensor4.setText(arr.get(3));
-                            t_sensor5.setText(arr.get(4));
-                            t_sensor6.setText(arr.get(5));
-
-
-                        }
-                });
-
             }
-        };
-        worker.start();
-=======
-    public void onStart(){
-        super.onStart();
->>>>>>> 3303cd871af0543efeadeab4e6ee8d507c5aabe0
-
-        Thread thread1 = new Thread(new Runnable(){
-           public void run(){
-
-               try{
-
-                   ArrayList<NameValuePair> NameValuePairs=new ArrayList<NameValuePair>(1);
-                   //보낼 값을 ArrayList에 키와 값으로 저장을 한다.
-                   NameValuePairs.add(new BasicNameValuePair("Mode","TouchSensor_Request"));
-                   getMethod.setEntity(new UrlEncodedFormEntity(NameValuePairs));
-                   //값을 보낸다.
-                   HttpResponse response = client.execute(getMethod);
-
-                   //값을 읽기 위한 cbr을 생성
-                   BufferedReader cbr=new BufferedReader(new InputStreamReader(response.getEntity().getContent(),"utf-8"));
-                   while((readData=cbr.readLine())!=null){
-                       arr.add("value : "+readData);
-                   }
-                   System.out.println("arr : " + arr);
-
-                   //
-
-                   Message msg = handler.obtainMessage();
-                   handler.sendMessage(msg);
-
-                   //
-
-               } catch(Exception e){
-                   e.printStackTrace();
-               }
-
-           }
 
         });
 
@@ -203,7 +134,7 @@ public class Frag_Realtime extends Fragment {
 
     //@Override
     public void onStop() {
-         super.onStop();
+        super.onStop();
 
         running = false;
 
