3303cd871af0543efeadeab4e6ee8d507c5aabe0
JungEunHye <zxy816@naver.com>
2015-05-11 17:47:21 +0900

    realtime_frag  revise

--- i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/Frag_Realtime.java
+++ i_keeper_test/app/src/main/java/org/androidtown/i_keeper_test/Frag_Realtime.java
@@ -2,6 +2,7 @@ package org.androidtown.i_keeper_test;
 
 
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -50,7 +51,20 @@ public class Frag_Realtime extends Fragment {
 
     private final Handler mHandler = new Handler();
     private Runnable mTimer1;
+//
+    private boolean running = false;
+    ProgressHandler handler;        //객체 생성
+    //
+    // 메인 스레드가 처리해야 할 루틴
 
+    //HttpClient 인스턴스 생성
+    HttpClient client=new DefaultHttpClient();
+    // HTTP 메서드의 인스턴스 생성
+    // URL과 함께 HttpClient 컴포넌트에서 제공하는 HTTP 메서드의 클래스의 생성자를 사용하여
+    // HTTP 의 요청라인을 지원하는 HTTP 메시지의 인스턴스를 생성한다.
+    HttpPost getMethod=new HttpPost("http://alert-height-91305.appspot.com/hello");
+
+    //
     public Frag_Realtime() {
         // Required empty public constructor
     }
@@ -60,6 +74,7 @@ public class Frag_Realtime extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
 
         View rootView =  inflater.inflate(R.layout.frag_realtime, container, false);
         user=new UserInfo();
@@ -71,81 +86,72 @@ public class Frag_Realtime extends Fragment {
         t_sensor5 = (TextView) rootView.findViewById(R.id.t_sensor5);
         t_sensor6 = (TextView) rootView.findViewById(R.id.t_sensor6);
 
-
-        Thread worker = new Thread() {
-            public void run() {
-                // 메인 스레드가 처리해야 할 루틴
-
-                //HttpClient 인스턴스 생성
-                HttpClient client=new DefaultHttpClient();
-                // HTTP 메서드의 인스턴스 생성
-                // URL과 함께 HttpClient 컴포넌트에서 제공하는 HTTP 메서드의 클래스의 생성자를 사용하여
-                // HTTP 의 요청라인을 지원하는 HTTP 메시지의 인스턴스를 생성한다.
-                HttpPost getMethod=new HttpPost("http://alert-height-91305.appspot.com/hello");
-
-                try{
-                    // 서버에 시간데이터를 보내고 해당 시간에 대한 센서값들을 얻어 온다.
-                    String inTime = new SimpleDateFormat("HH:mm:ss").format(new java.util.Date());  // 현재 시간을 얻어옴(시,분,초)
-                    String []tmp = inTime.split(":");
-                    int time_hour = Integer.parseInt(tmp[0]);           // 현재 시간(시)
-                    int time_minute = Integer.parseInt(tmp[1]);     // 현재 시간(분)
-                    output.setText("현재시간  : "+time_hour+"시 "+time_minute+"분");
-
-                    ArrayList<NameValuePair> NameValuePairs=new ArrayList<NameValuePair>(1);
-                    //보낼 값을 ArrayList에 키와 값으로 저장을 한다.
-                    NameValuePairs.add(new BasicNameValuePair("Mode","TouchSensor_Request"));
-                    getMethod.setEntity(new UrlEncodedFormEntity(NameValuePairs));
-                    //값을 보낸다.
-                    HttpResponse response = client.execute(getMethod);
-
-                    //값을 읽기 위한 cbr을 생성
-                    BufferedReader cbr=new BufferedReader(new InputStreamReader(response.getEntity().getContent(),"utf-8"));
-                    while((readData=cbr.readLine())!=null){
-                                arr.add("value : "+readData);
-                    }
-                    System.out.println("arr : "+ arr);
-<<<<<<< HEAD
-=======
-                    t_sensor1.setText(arr.get(0));
-                    t_sensor2.setText(arr.get(1));
-                    t_sensor3.setText(arr.get(2));
-                    t_sensor4.setText(arr.get(3));
-                    t_sensor5.setText(arr.get(4));
-                    t_sensor6.setText(arr.get(5));
->>>>>>> 99dd66c7282d17eb417dd214bc846cf1558e1586
-
-                } catch(Exception e){
-                    e.printStackTrace();
-                }
-
-                mHandler.post(new Runnable(){
-                        @Override
-                        public void run(){
-                            t_sensor1.setText(arr.get(0));
-                            t_sensor2.setText(arr.get(1));
-                            t_sensor3.setText(arr.get(2));
-                            t_sensor4.setText(arr.get(3));
-                            t_sensor5.setText(arr.get(4));
-                            t_sensor6.setText(arr.get(5));
-
-
-                        }
-                });
-
-            }
-        };
-        worker.start();
-
-
+        handler = new ProgressHandler();
 
         return rootView;
         //  return inflater.inflate(R.layout.frag_realtime, container, false);
     }
-    //@Override
-    public void onPause() {
-        mHandler.removeCallbacks(mTimer1);
 
-        super.onPause();
+
+    public void onStart(){
+        super.onStart();
+
+        Thread thread1 = new Thread(new Runnable(){
+           public void run(){
+
+               try{
+
+                   ArrayList<NameValuePair> NameValuePairs=new ArrayList<NameValuePair>(1);
+                   //보낼 값을 ArrayList에 키와 값으로 저장을 한다.
+                   NameValuePairs.add(new BasicNameValuePair("Mode","TouchSensor_Request"));
+                   getMethod.setEntity(new UrlEncodedFormEntity(NameValuePairs));
+                   //값을 보낸다.
+                   HttpResponse response = client.execute(getMethod);
+
+                   //값을 읽기 위한 cbr을 생성
+                   BufferedReader cbr=new BufferedReader(new InputStreamReader(response.getEntity().getContent(),"utf-8"));
+                   while((readData=cbr.readLine())!=null){
+                       arr.add("value : "+readData);
+                   }
+                   System.out.println("arr : " + arr);
+
+                   //
+
+                   Message msg = handler.obtainMessage();
+                   handler.sendMessage(msg);
+
+                   //
+
+               } catch(Exception e){
+                   e.printStackTrace();
+               }
+
+           }
+
+        });
+
+        thread1.start();
     }
 
+
+    //@Override
+    public void onStop() {
+         super.onStop();
+
+        running = false;
+
+    }
+
+    public class ProgressHandler extends Handler{ //Handler 클래스를 상속하여 새로운 핸들러 클래스를 정의
+        public void handleMessage(Message msg){
+            // 여기서 setTExt
+            t_sensor1.setText(arr.get(0));
+            t_sensor2.setText(arr.get(1));
+            t_sensor3.setText(arr.get(2));
+            t_sensor4.setText(arr.get(3));
+            t_sensor5.setText(arr.get(4));
+            t_sensor6.setText(arr.get(5));
+
+        }
+    }
 }
