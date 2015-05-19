b871d486eb1abfde5ca248f828b5d87fdd91664e
jeongdongwon <ch0703_kr@naver.com>
2015-05-20 08:11:28 +0900

    스마트의류 최종코드, 옷센서 검사용 소스 추가

---
+++ Arduino/all5/all5.ino
@@ -0,0 +1,205 @@
+#define arr_max 20
+#define fil_max 4
+#define SHAKE_SPAPE 4
+#define PULSE_SPAPE 1
+#define LINE_SPAPE 0
+#define press_arr_max 150
+
+int press_arr[4][press_arr_max];
+int arr_count = 0;
+
+//운동오류 관련된 변수들
+bool is_running = false;
+int heart_beat;
+int error_thresh =17;
+int error_count = 0;
+
+//센서 입력관련 변수들
+int p_s1, p_s2, p_s3, p_s4;
+int h_s1 = 0;
+int heart_arr[arr_max+10] = {0};
+int heart_count = 0;
+int filter_arr[fil_max];
+int filter_count =0 ;
+
+int analysis_con;
+
+//시간관련 함수들
+unsigned long int prev_time;
+int time_gap;
+
+void setup() { 
+  Serial.begin(9600);	
+  while(heart_count < arr_max+10){          
+    heart_arr[heart_count++] = analogRead(A2);
+  }
+  prev_time = millis();
+}
+
+void loop() {
+  p_s1 = 1024-analogRead(A0);
+  p_s2 = 1024-analogRead(A1);
+  p_s3 = 1024-analogRead(A3);
+  p_s4 = 1024-analogRead(A4);
+  h_s1 = analogRead(A2);
+  
+  insert_to_arr(h_s1);
+  heart_beat = peak_emphasis(arr_max-2);
+  insert_to_filter_arr(heart_beat);  
+  /*Serial.println(heart_beat);*/
+ 
+  if(filter_count >= fil_max){
+    //Serial.println(error_count);
+    analysis_con = analysis();
+    bool temp = is_running;
+    is_running = (error_count>error_thresh)? true: false;
+    if(temp != is_running){
+     prev_time = millis();
+    if(is_running){ error_count+=20;} 
+   }
+    
+    if(is_running){
+      //Serial.println("i'm running");
+      if(analysis_con >= SHAKE_SPAPE ){
+        //Serial.println("i'm running-shake");
+        error_count  = (error_count+5 > 70)? 70 : error_count+5;        
+      }
+      else{
+        //Serial.println("i'm running-line");
+        error_count = (error_count-1 < 0)? 0 : error_count-1;        
+        }
+        
+//        Serial.print(millis());
+//         Serial.print(" ");
+//         Serial.println(prev_time);
+       if(millis() - prev_time > 300){         
+          Serial.print("0 ");
+          sort_press_arr();
+          send_press_top3(); 
+          prev_time = millis();
+      }
+    }
+    else{
+      //Serial.println("i'm standing");
+      //Serial.println(analysis_con);
+      if(analysis_con >= SHAKE_SPAPE){
+        //Serial.println("i'm standing - shake");
+        error_count  = (error_count+5 > 70)? 70 : error_count+5;
+      }
+      else if(analysis_con == PULSE_SPAPE){
+        int temp_time = millis()-prev_time;
+        if( temp_time < 400 || temp_time > 1300){
+          //Serial.println("i'm standing-one. but too shoert");
+          error_count  = (error_count+5 > 70)? 70 : error_count+5;
+          prev_time = millis();
+        }        
+        else{
+          //Serial.println("==============================i'm standing-one");
+          error_count = (error_count-1 < 0)? 0 : error_count-1;          
+          Serial.print(temp_time);
+          Serial.print(" ");
+          sort_press_arr();
+          send_press_top3();        
+          prev_time = millis();        
+        }
+      }
+      else{
+        //Serial.println("i'm standing-line");
+        error_count = (error_count-1 < 0)? 0 : error_count-1;     
+      }
+    }
+    filter_count=0;
+  }
+  press_arr[0][arr_count] = p_s1;
+  press_arr[1][arr_count] = p_s2;
+  press_arr[2][arr_count] = p_s3;
+  press_arr[3][arr_count] = p_s4;
+  arr_count = (arr_count+1>press_arr_max-1)?0:arr_count+1; 
+ delay(7);
+}
+
+int analysis(){
+  int count = 0;
+  for(int i=0; i<fil_max; i++)
+    if(filter_arr[i]>0)
+      count++;
+     
+  if( count == 0)   return LINE_SPAPE;  
+  else if( count < 3) return PULSE_SPAPE; 
+  else return count;
+}
+
+int peak_emphasis(int input){
+  int temp = 0;
+  temp = 8*(heart_arr[input]-heart_arr[input-1]);
+  if(temp<500) temp =0;
+  if(temp >1023) { temp = 1023; }
+  return temp;
+}
+
+void insert_to_arr(int input){
+  for(int i=0; i<arr_max+10; i++){
+    heart_arr[i] = heart_arr[i+1];
+  }
+  heart_arr[arr_max+9]  = input;
+}
+
+void insert_to_filter_arr(int input){
+//   for(int i=0; i<fil_max; i++){
+//    Serial.print(filter_arr[i]);
+//    Serial.print(" ");
+//  }
+//   Serial.println(" ");
+
+//Serial.print(filter_arr[0]);Serial.print(" ");
+//Serial.print(filter_arr[1]);Serial.print(" ");
+//Serial.print(filter_arr[2]);Serial.print(" ");
+//Serial.print(filter_arr[3]);Serial.print(" ");
+//Serial.print(filter_arr[4]);Serial.print(" ");
+//Serial.print(filter_arr[5]);Serial.println(" ");
+
+
+  filter_count++;
+  for(int i=0; i<fil_max; i++){
+    filter_arr[i] = filter_arr[i+1];
+  }
+  filter_arr[fil_max-1]  = input;
+}
+
+void sort_press_arr(){
+  int top, temp; 
+  for(int i=0; i<4; i++){
+    top = press_arr[i][0];
+    for(int j=1; j<arr_count; j++){
+      if(top < press_arr[i][j]){
+        top = press_arr[i][j];
+      }
+    }
+    press_arr[i][arr_count]=top;
+  }
+  
+//  for(int i=0; i<4; i++){
+//    for(int j=0; j<arr_count; j++){
+//      for(int k=1; k<arr_count; k++){
+//        int temp;
+//        if(press_arr[i][j]>press_arr[i][j]){
+//          temp = press_arr[i][j];
+//          press_arr[i][j] = press_arr[i][k];
+//          press_arr[i][k] = temp;
+//        }	
+//      }
+//    }
+//  }
+  
+  
+}
+void send_press_top3(){
+  double top_3_temp = arr_count * 1.0;
+  int top_3 = top_3_temp;
+  for(int i=0; i<4; i++){
+    Serial.print(press_arr[i][top_3]);
+    Serial.print(" ");
+  }
+  Serial.println(" ");
+  arr_count=0;
+}
---
+++ Arduino/show/show.pde
@@ -0,0 +1,105 @@
+import processing.serial.*; 
+ 
+char interest = ' ';
+Serial myPort;        // The serial port 
+int xPos = 1;         // horizontal position of the graph 
+float height_old1 = 0; 
+float height_old2 = 0; 
+float height_old3 = 0; 
+float height_old4 = 0; 
+float height_old5 = 0; 
+float height_old6 = 0; 
+
+float height_new = 0; 
+float inByte = 0; 
+
+void setup () { 
+  // set the window size: 
+  size(1000, 800); 
+  println(Serial.list()); 
+  // Open whatever port is the one you're using. 
+  myPort = new Serial(this, Serial.list()[0], 9600); 
+  // don't generate a serialEvent() unless you get a newline character: 
+  myPort.bufferUntil('\n'); 
+  background(0xff); 
+} 
+
+void draw (){
+}
+ 
+void serialEvent (Serial myPort) { 
+  // get the ASCII string: 
+  String inString1 = myPort.readStringUntil(interest); 
+  String inString2 = myPort.readStringUntil(interest); 
+  String inString3 = myPort.readStringUntil(interest); 
+  String inString4 = myPort.readStringUntil(interest); 
+  String inString5 = myPort.readStringUntil(interest); 
+  String inString6 = myPort.readStringUntil('\n'); 
+  stroke(0, 0, 0);
+  line(1, height*2/3, width-1, height*2/3); 
+  line(1, height/3, width-1, height/3); 
+//  print(inString1);  print(" "); 
+//  print(inString2);  print(" ");  
+//  print(inString3);  print(" ");  
+//  print(inString4);  print(" "); 
+//  print(inString5);  print(" "); 
+//  print(inString6);  print(" "); 
+
+  inString1 = trim(inString1); 
+  stroke(0xff, 0, 0); //Set stroke to red ( R, G, B) 
+  inByte = float(inString1);  
+  inByte = map(inByte, 0, 1023, height*2/3, height); 
+  height_new = height - inByte;  
+  line(xPos - 1, height_old1, xPos, height_new); 
+  height_old1 = height_new; 
+  
+  inString2 = trim(inString2); 
+  stroke(0, 0xff, 0); //Set stroke to red ( R, G, B) 
+  inByte = float(inString2);  
+  inByte = map(inByte, 0, 1023,  height/3, height*2/3); 
+  height_new = height - inByte;  
+  line(xPos - 1, height_old2, xPos, height_new); 
+  height_old2 = height_new; 
+  
+  inString3 = trim(inString3); 
+  stroke(0, 0, 0xff); //Set stroke to red ( R, G, B) 
+  inByte = float(inString3);  
+  inByte = map(inByte, 0, 1023, 0, height/3); 
+  height_new = height - inByte;  
+  line(xPos - 1, height_old3, xPos, height_new); 
+  height_old3 = height_new; 
+  
+  inString4 = trim(inString4); 
+  stroke(0xff, 120, 0); //Set stroke to red ( R, G, B) 
+  inByte = float(inString4);  
+  inByte = map(inByte, 0, 1023, 0, height/3); 
+  height_new = height - inByte;  
+  line(xPos - 1, height_old4, xPos, height_new); 
+  height_old4 = height_new; 
+  
+  inString5 = trim(inString5); 
+  stroke(0, 0xff, 0xff); //Set stroke to red ( R, G, B) 
+  inByte = float(inString5);  
+  inByte = map(inByte, 0, 1023, 0, height/3); 
+  height_new = height - inByte;  
+  line(xPos - 1, height_old5, xPos, height_new); 
+  height_old5 = height_new; 
+  
+  inString6 = trim(inString6); 
+  stroke(0xff, 0, 0); //Set stroke to red ( R, G, B) 
+  inByte = float(inString6);  
+  inByte = map(inByte, 0, 1023, 0, height/3); 
+  height_new = height - inByte;  
+  line(xPos - 1, height_old6, xPos, height_new); 
+  height_old6 = height_new; 
+  
+  if (xPos >= width) { 
+    xPos = 0; 
+    background(0xff); 
+  }  
+  else { 
+    xPos+=2; 
+  }      
+} 
+
+
